2927c7dc6fa24a2df5deed6d4b9394c904be054f
Rin-bamboo <Rin-bamboo@users.noreply.github.com>
2025-07-08 14:28:48 +0000

    deploy: 5c4e4abe7ba15ef0c66a03568535e20ad47f1bf9

---
+++ service-worker.published.js
@@ -0,0 +1,17 @@
+﻿// wwwroot/service-worker.published.js
+
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.open('blazor-cache').then(cache => {
+            return cache.match(event.request).then(response => {
+                return response || fetch(event.request).then(fetchResponse => {
+                    cache.put(event.request, fetchResponse.clone());
+                    return fetchResponse;
+                });
+            });
+        })
+    );
+});
